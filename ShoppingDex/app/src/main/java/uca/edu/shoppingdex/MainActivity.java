3e82bc095624579920d0881c15bf4aced3f066af
Shiroe-sensei <74565295+Shiroe-sensei@users.noreply.github.com>
2020-11-23 15:12:37 -0600

    Add files via upload

---
+++ ShoppingDex/app/src/main/java/uca/edu/shoppingdex/MainActivity.java
@@ -0,0 +1,14 @@
+package uca.edu.shoppingdex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
